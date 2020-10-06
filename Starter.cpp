ab0629ad58ea7d48b4653322347683f5c2553afb
Programingisfun <72470137+Programingisfun@users.noreply.github.com>
2020-10-06 15:19:31 -0500

    Add files via upload
    This I just a helpful way to remind how to step up your starter code for a c++ program so you can just use this  copy and paste for starter programs

---
+++ Starter.cpp
@@ -0,0 +1,16 @@
+// Description of the program
+// Author
+
+// Preprocessor Directives
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+	// Keep this stuff at the bottom.
+	cout << endl;
+	system("pause");
+	return 0;
+}
